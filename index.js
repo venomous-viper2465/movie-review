62b92a66fd22aba432756513b3e0d2f527ef7b5c
nishad <muhammed.mca23.du@gmail.com>
2025-01-03 22:34:13 +0530

    get the list of genres by id and list of genres http get request  to get genre by id or all of the genres

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const app = express();
+const genres = [
+  { id: 1, name: "action" },
+  { id: 2, name: "mystery" },
+  { id: 3, name: "sci-fi" },
+];
+app.get("/movie/api/genres", (req, res) => {
+  let categories = [];
+  for (const x in genres) {
+    categories.push(genres[x].name);
+  }
+  res.send(categories);
+});
+
+app.get("/movie/api/genres/:id", (req, res) => {
+  const index = parseInt(req.params.id);
+  const genre = genres.find((x) => x.id === index);
+  if (!genre)
+    return res.status(404).send("The specified resource could not be found");
+
+  res.send(genre);
+});
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
